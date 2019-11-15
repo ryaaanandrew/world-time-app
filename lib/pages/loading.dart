362c8a27b69d7e6e48ca41af3693644534010bfc
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-11-14 21:38:31 -0500

    d

---
+++ lib/pages/loading.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    Response response = await get('http://worldtimeapi.org/api/timezone/America/Toronto');
+    Map data = json.decode(response.body);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen'),
+    );
+  }
+}
